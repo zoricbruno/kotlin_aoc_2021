8e39d2b65039f82cf6d3fdc8cfe7c9c03a84f221
Bruno <bruno.ferit@gmail.com>
2021-12-02 08:19:34 +0100

    Created initial structure for day02 of KOAC

---
+++ src/day02/Day02.kt
@@ -0,0 +1,22 @@
+package day02
+
+import readInput
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("02", "Day02_test")
+    check(part1(testInput) == 150)
+    // check(part2(testInput) == 0)
+
+    val input = readInput("02", "Day02")
+    println("Part 1: ${part1(input)}")
+    // println("Part 2: ${part2(input)}")
+}
+
+fun part1(input: List<String>): Int {
+    // TODO
+}
+
+fun part2(input: List<String>): Int {
+    //TODO
+}
