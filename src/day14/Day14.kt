6c5f62b136a01a4e4a883559e3f29fd381e0ddab
Bruno <bruno.zoric@ferit.hr>
2021-12-14 05:29:46 +0100

    Prepared for day14

---
+++ src/day14/Day14.kt
@@ -0,0 +1,23 @@
+package day14
+
+import readInput
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("14", "Day14_test")
+    check(part1(testInput) ==0)
+    check(part2(testInput) == 16)
+
+    val input = readInput("14", "Day14")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
+
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int {
+    return 0
+}
+
